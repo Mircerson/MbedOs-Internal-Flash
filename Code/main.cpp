a25ea269bdabc31aa205c4c1a3c25b3bee1eecc0
Mircea-Iuliu Micle <83925805+Mircerson@users.noreply.github.com>
2022-03-22 01:47:56 +0000

    Cleanup

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,18 +1,5 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include "mbed.h"
-#include <inttypes.h>
-
-
-
 #include "FlashMemory.hpp"
-// Blinking rate in milliseconds
-
-
-//FlashIAP flash;
 
 
 FlashMemory internalFlash;
@@ -27,6 +14,5 @@ int main()
     myData.len = 3;
     internalFlash.writeMemory(myData, 0x08040000);
     internalFlash.readMemory(0x08040000, 3);
-    while (true) {
-    }
+    while (true) {};
 }
